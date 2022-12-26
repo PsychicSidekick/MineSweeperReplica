8e29ad27bb9bd2071b555091317d9f069568d297
PsychicSidekick <scchanethan@gmail.com>
2022-12-26 23:19:59 +0800

    Add multiple methods for basic gameplay
    Reveal connecting tiles, Reveal adjacent tiles, Mark number of nearby mines, Flag mines.

--- Mine Sweeper Replica/Assets/Scripts/Tile.cs
+++ Mine Sweeper Replica/Assets/Scripts/Tile.cs
@@ -2,19 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IPointerClickHandler
 {
+    public Image img;
+    public TMP_Text txt;
+
     public Vector2Int pos;
-    public Button btn;
-    public bool revealed;
+    public bool revealed = false;
+    public int adjacentMineCount;
     public bool mined;
+    public bool flagged = false;
+    public List<GameObject> adjacentTileObjs;
+
+    private void Start()
+    {
+        adjacentTileObjs = GetAdjacentTiles(this);
+        adjacentMineCount = CountAdjacentMines(adjacentTileObjs);
+    }
 
     public void Reveal()
     {
-        foreach (GameObject tileObj in GetAdjacentTiles(this))
+        if (mined)
         {
-            Debug.Log(tileObj.GetComponent<Tile>().pos);
+            return;
+        }
+
+        if (adjacentMineCount > 0)
+        {
+            txt.text = adjacentMineCount.ToString();
+            revealed = true;
+            img.color = Color.green;
+            return;
+        }
+
+        revealed = true;
+        img.color = Color.green;
+
+        foreach (GameObject tileObj in adjacentTileObjs)
+        {
+            if (tileObj.GetComponent<Tile>().revealed)
+            {
+                continue;
+            }
+            tileObj.GetComponent<Tile>().Reveal();
+        }
+    }
+
+    public void RevealAdjacent()
+    {
+        foreach (GameObject tileObj in adjacentTileObjs)
+        {
+            Tile tile = tileObj.GetComponent<Tile>();
+
+            tile.Reveal();
+        }
+    }
+
+    public void FlagMine()
+    {
+        if(flagged)
+        {
+            flagged = false;
+            img.color = Color.white;
+        }
+        else
+        {
+            flagged = true;
+            img.color = Color.red;
         }
     }
 
@@ -28,7 +85,7 @@ public class Tile : MonoBehaviour
         {
             for (int j = -1; j < 2; j++)
             {
-                if(i == 0 && j == 0)
+                if (i == 0 && j == 0)
                 {
                     continue;
                 }
@@ -36,7 +93,7 @@ public class Tile : MonoBehaviour
                 int xPos = tile.pos.x + i;
                 int yPos = tile.pos.y + j;
                 
-                if(xPos < 0 || xPos > boardSize.x || yPos < 0 || yPos > boardSize.y)
+                if (xPos < 0 || xPos >= boardSize.x || yPos < 0 || yPos >= boardSize.y)
                 {
                     continue;
                 }
@@ -48,9 +105,36 @@ public class Tile : MonoBehaviour
         return adjacentTiles;
     }
 
-    public void OnClick()
+    public int CountAdjacentMines(List<GameObject> adjacentTiles)
     {
-        Reveal();
-        btn.interactable = false;
+        int mineCount = 0;
+
+        foreach (GameObject tile in adjacentTiles)
+        {
+            if (tile.GetComponent<Tile>().mined == true)
+            {
+                mineCount++;
+            }
+        }
+
+        return mineCount;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (revealed)
+        {
+            RevealAdjacent();
+            return;
+        }
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            Reveal();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            FlagMine();
+        }
     }
 }
